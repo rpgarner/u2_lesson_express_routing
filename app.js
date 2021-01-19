aaccbab23da4a1cae3b60fea474a597a2994f31b
ahonore42 <a1honore80@gmail.com>
2021-01-19 09:57:24 -0600

    add express routing lesson

---
+++ app.js
@@ -0,0 +1,10 @@
+const { request, response } = require('express')
+const express = require('express')
+const PORT = process.env.PORT || 3001
+
+const app = express()
+//  Your Code Here
+
+
+// End Your Code Here
+app.listen(PORT, () => console.log(`Server Listening on port: ${PORT}`))
